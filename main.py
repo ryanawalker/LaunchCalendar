c20d0883305d038e11508309ee78e4daea18c18a
Ryan Walker <ryan@launchcode.org>
2018-08-29 14:19:22 -0500

    Adds persistence, persistent Event class. Week view still not persistent.

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-from flask import request, url_for
+from flask import request, redirect, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 from flask_api import FlaskAPI, status, exceptions
 import datetime
 
-app = FlaskAPI(__name__)
 
+app = FlaskAPI(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://launchcalendar:launch@localhost:8889/launchcalendar'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Event(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    date = db.Column(db.Date)
+    calendar = db.Column(db.String(120))
+
+    def __init__(self, calendar: str, name: str, date: str):
+        self.calendar = calendar
+        self.name = name
+        self.date = datetime.date.fromisoformat(date)
+
+    def get_JSON(self):
+        return {
+            'url': "https" + request.host_url.lstrip('http').rstrip('/') + url_for('dates_detail', key=self.id),
+            "calendar": self.calendar,
+            "name": self.name,
+            "date": self.date
+        }
 
 dates = {
     0: {"calendar": "LC101", "name": "Ryan", "date": datetime.date.fromisoformat("2018-05-22")},
@@ -33,14 +59,16 @@ def dates_list():
         name = data_list[1]
         date = data_list[2]
         if calendar != '' and name != '' and date != '':
-            idx = max(dates.keys()) + 1
-            dates[idx] = {"calendar": calendar, "name": name, "date": datetime.date.fromisoformat(date)}
-            return date_repr(idx), status.HTTP_201_CREATED
+            event = Event(calendar, name, date)
+            db.session.add(event)
+            db.session.commit()
+            return event.getJSON(), status.HTTP_201_CREATED
         else:
             return {"error": "calendar, date, and  name required"}, status.HTTP_400_BAD_REQUEST
 
     # request.method == 'GET'
-    return [date_repr(idx) for idx in sorted(dates.keys())]
+    events = Event.query.all()
+    return [event.get_JSON() for event in events]
 
 
 @app.route("/<int:key>/", methods=['GET', 'PUT', 'DELETE'])
